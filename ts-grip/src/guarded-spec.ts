560873575529fa3834092735e172e19d4997d687
Andy Peterson <andy@ndpsoftware.com>
2025-05-05 11:55:55 -0700

    fix guardedGrip and handle async/sync magically

--- ts-grip/src/guarded-spec.ts
+++ ts-grip/src/guarded-spec.ts
@@ -1,91 +1,179 @@
-import {describe, it as spec} from "node:test";
+import {beforeEach, describe, it as spec} from "node:test";
 import assert from "node:assert/strict";
-import {guardedGrip} from "./guarded";
-import {valueGrip} from "./valueGrip";
+import {AsyncGuard, guardedGrip, guardedGripAsync, guardedGripSync, SyncGuard} from "./guarded";
+import {ValueGrip, valueGrip} from "./valueGrip";
+import {ManualGrip} from "./manualGrip";
+import {Grip} from "./grip";
 
-describe('guardedGrip', () => {
-  spec('should use source grip value when guard function returns false', () => {
-    const srcGrip = valueGrip(0);
-    const guarded = valueGrip(1);
-    const guardFn = () => false;
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+describe('guardedGripSync', () => {
 
-    assert.equal(grip.value, 0);
+  let srcGrip
+  let guarded
+
+  describe('sync', () => {
+    beforeEach(() => {
+      srcGrip = valueGrip(0);
+      guarded = valueGrip(1);
+    });
+    [
+      [(() => false), 'srcGrip', 0, 2, 1],
+      [(() => true), 'guarded', 1, 2, 2],
+      [valueGrip(false), 'srcGrip', 0, 2, 1],
+      [valueGrip(true), 'guarded', 1, 2, 2]
+    ].forEach(([guardFn, source, expected, srcValueExpected, guardedValueExpected]:
+               [Grip<boolean>, 0 | 1, 'srcGrip' | 'guarded', 0 | 1 | 2, 0 | 1 | 2]) => {
+
+      spec(`guardedGripSync should use ${source} when guard ${guardFn}`, () => {
+        const grip = guardedGripSync(srcGrip, guardFn, guarded);
+        assert.equal(grip.value, expected);
+      });
+
+      spec(`guardedGripSync should set value on ${source} when guard ${guardFn}`, () => {
+        const grip = guardedGripSync(srcGrip, guardFn, guarded);
+        grip.set(2);
+        assert.equal(srcGrip.value, srcValueExpected);
+        assert.equal(guarded.value, guardedValueExpected);
+        assert.equal(grip.value, 2)
+      });
+
+      spec(`guardedGrip should use ${source} when guard ${guardFn}`, () => {
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+        assert.equal(grip.value, expected);
+      });
+
+      spec(`guardedGrip should set value on ${source} when guard ${guardFn}`, () => {
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+        grip.set(2);
+        assert.equal(srcGrip.value, srcValueExpected);
+        assert.equal(guarded.value, guardedValueExpected);
+        assert.equal(grip.value, 2)
+      });
+
+    });
   });
 
-  spec('should use guarded grip value when guard function returns true', () => {
-    const srcGrip = valueGrip(0);
-    const guarded = valueGrip(1);
-    const guardFn = () => true;
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+  describe('async', () => {
+    beforeEach(() => {
+      srcGrip = valueGrip(Promise.resolve(0));
+      guarded = valueGrip(Promise.resolve(1));
+    });
 
-    assert.equal(grip.value, 1);
-  });
+    [
+      [false, 'srcGrip', 0, 2, 1],
+      [true, 'guarded', 1, 2, 2],
+    ].forEach(([guard, source, expected, srcValueExpected, guardedValueExpected]:
+               [boolean, 0 | 1, 'srcGrip' | 'guarded', 0 | 1 | 2, 0 | 1 | 2]) => {
 
-  spec('should set value on source grip when guard function returns false', () => {
-    const srcGrip = valueGrip(0);
-    const guarded = valueGrip(1);
-    const guardFn = () => false;
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+      spec(`guardedGripAsync should use ${source} when async guard function returns ${guard}`, async () => {
+        const guardFn = () => Promise.resolve(guard);
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
 
-    grip.set(2);
-    assert.equal(srcGrip.value, 2);
-    assert.equal(guarded.value, 1);
-  });
+        assert.equal(await grip.value, expected);
+      });
 
-  spec('should set value both grips when guard function returns true', () => {
-    const srcGrip = valueGrip(0);
-    const guarded = valueGrip(1);
-    const guardFn = () => true;
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+      spec(`guardedGripAsync should set value on ${source} when async guard function returns ${guard}`, async () => {
+        const guardFn = () => Promise.resolve(guard)
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
 
-    grip.set(2);
-    assert.equal(srcGrip.value, 2);
-    assert.equal(guarded.value, 2);
-  });
-  //
-  // spec('should handle async guard function', async () => {
-  //   const srcGrip = valueGrip(0);
-  //   const guarded = valueGrip(1);
-  //   const guardFn = async () => true;
-  //   const grip = guardedGrip(srcGrip, guardFn, guarded);
-  //
-  //   assert.equal(await grip.value, 1);
-  // });
+        await grip.set(Promise.resolve(2));
 
-  spec('should handle async getter and setter with guard on', async () => {
-    const srcGrip = valueGrip(Promise.resolve(0));
-    const guarded = valueGrip(Promise.resolve(1));
-    const guardFn = () => true;
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
 
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+      spec(`guardedGripAsync should use ${source} when async guard grips ${guard}`, async () => {
+        const guardFn = valueGrip(guard);
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
 
-    assert.equal(await grip.value, 1);
-    assert.equal(await srcGrip.value, 0);
-    assert.equal(await guarded.value, 1);
+        assert.equal(await grip.value, expected);
+      });
 
-    await grip.set(Promise.resolve(2))
+      spec(`guardedGripAsync should set value on ${source} when async guard grips ${guard}`, async () => {
+        const guardFn = valueGrip(guard);
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
 
-    assert.equal(await grip.value, 2)
-    assert.equal(await srcGrip.value, 2);
-    assert.equal(await guarded.value, 2);
-  });
-  spec('should handle async getter and setter with guard off', async () => {
+        await grip.set(Promise.resolve(2));
 
-    const srcGrip = valueGrip(Promise.resolve(0));
-    const guarded = valueGrip(Promise.resolve(1));
-    const guardFn = () => false;
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
 
-    const grip = guardedGrip(srcGrip, guardFn, guarded);
+      spec(`guardedGripAsync should use ${source} when async guard grips promise of ${guard}`, async () => {
+        const guardFn = valueGrip(Promise.resolve(guard));
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
 
-    assert.equal(await guarded.value, 1); // ignored
-    assert.equal(await grip.value, 0)
-    assert.equal(await srcGrip.value, 0);
+        assert.equal(await grip.value, expected);
+      });
 
-    await grip.set(Promise.resolve(2))
-    assert.equal(await grip.value, 2)
-    assert.equal(await srcGrip.value, 2);
-    assert.equal(await guarded.value, 1); // doesn't change
-  });
+      spec(`guardedGripAsync should set value on ${source} when async guard grips promise of ${guard}`, async () => {
+        const guardFn = valueGrip(Promise.resolve(guard));
+        const grip = guardedGripAsync(srcGrip, guardFn, guarded);
+
+        await grip.set(Promise.resolve(2));
+
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
+
+      spec(`guardedGrip should use ${source} when async guard function returns ${guard}`, async () => {
+        const guardFn = () => Promise.resolve(guard);
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        assert.equal(await grip.value, expected);
+      });
+
+      spec(`guardedGrip should set value on ${source} when async guard function returns ${guard}`, async () => {
+        const guardFn = () => Promise.resolve(guard)
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        await grip.set(Promise.resolve(2));
+
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
+
+      spec(`guardedGrip should use ${source} when async guard grips ${guard}`, async () => {
+        const guardFn = valueGrip(guard);
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        assert.equal(await grip.value, expected);
+      });
+
+      spec(`guardedGrip should set value on ${source} when async guard grips ${guard}`, async () => {
+        const guardFn = valueGrip(guard);
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        await grip.set(Promise.resolve(2));
+
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
+
+      spec(`guardedGrip should use ${source} when async guard grips promise of ${guard}`, async () => {
+        const guardFn = valueGrip(Promise.resolve(guard));
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        assert.equal(await grip.value, expected);
+      });
+
+      spec(`guardedGrip should set value on ${source} when async guard grips promise of ${guard}`, async () => {
+        const guardFn = valueGrip(Promise.resolve(guard));
+        const grip = guardedGrip(srcGrip, guardFn, guarded);
+
+        await grip.set(Promise.resolve(2));
+
+        assert.equal(await srcGrip.value, srcValueExpected);
+        assert.equal(await guarded.value, guardedValueExpected);
+        assert.equal(await grip.value, 2)
+      });
+
+    })
+  })
+
 });
 
--- ts-grip/src/guarded.ts
+++ ts-grip/src/guarded.ts
@@ -1,16 +1,93 @@
 import {Grip} from "./grip";
 import {manualGrip} from "./manualGrip";
 
-export function guardedGrip<T>(srcGrip: Grip<T>, guardFn: () => boolean, guarded: Grip<T>) {
-  const grip = manualGrip(
-    () => guardFn() ? guarded.value : srcGrip.value,
-    (newValue) => {
-      if (guardFn()) {
-        srcGrip.set(newValue)
-        return guarded.set(newValue)
-      }
-      return srcGrip.set(newValue)
-    }
-  )
-  return grip
+
+function isGrip<T>(value: unknown): value is Grip<T> {
+  return value instanceof Grip<T>;
 }
+
+/*
+An `isPromise` detector that narrows the type of the promise return type,
+when returning `true`.
+ */
+export function isPromise<T = any>(obj: any):
+  obj is T extends { then: (...args: unknown[]) => unknown } ? Promise<Awaited<T>> : never {
+  return !!obj &&
+    (typeof obj === 'object' || typeof obj === 'function') &&
+    typeof obj.then === 'function';
+}
+
+function isFunction(obj: unknown): obj is Function {
+  return typeof obj === 'function'
+}
+
+function isSyncGuard(obj: any): obj is SyncGuard {
+  return (isGrip(obj) && typeof obj.value === 'boolean')
+    || (isFunction(obj) && !isPromise(obj()))
+}
+
+export function guardedGrip<T extends unknown,
+  GT extends Grip<unknown> = (Grip<T> | Grip<Promise<T>>)>(
+  srcGrip: GT,
+  guard: AsyncGuard | SyncGuard,
+  guarded: GT
+) {
+  if (isSyncGuard(guard)
+    && !isPromise(srcGrip.value)
+    && !isPromise(guarded.value))
+    return guardedGripSync(srcGrip, guard, guarded)
+  else
+    return guardedGripAsync(srcGrip, guard, guarded)
+}
+
+export type SyncGuard = (() => boolean) | Grip<boolean>
+
+// Synchronous version
+export function guardedGripSync<T>(
+  srcGrip: Grip<T>,
+  guard: SyncGuard,
+  guarded: Grip<T>
+) {
+  const resolveGuard =
+    () => typeof guard === 'function' ? guard() : guard.value;
+
+
+  return manualGrip(
+    () => (resolveGuard() ? guarded.value : srcGrip.value),
+    (newValue) => {
+      if (resolveGuard()) {
+        srcGrip.set(newValue);
+        return guarded.set(newValue);
+      }
+      return srcGrip.set(newValue);
+    }
+  );
+}
+
+export type AsyncGuard = (() => boolean | Promise<boolean>) | Grip<boolean | Promise<boolean>>
+
+// Asynchronous version
+export function guardedGripAsync<T>(
+  srcGrip: Grip<T|Promise<T>>,
+  guard: AsyncGuard,
+  guarded: Grip<T|Promise<T>>
+) {
+  const resolveGuard =
+    () => typeof guard === 'function' ? guard() : guard.value;
+
+  return manualGrip(
+    async () => {
+      const isGuarded = resolveGuard();
+      return (await isGuarded ? guarded.value : srcGrip.value);
+    },
+    async (newValue) => {
+      const isGuarded = await resolveGuard();
+      if (isGuarded) {
+        await srcGrip.set(newValue);
+        return guarded.set(newValue);
+      }
+      return srcGrip.set(newValue);
+    }
+  );
+}
+
