9e1c093f531d9651ef4188380253a471d3b9c372
Andy Peterson <andy@ndpsoftware.com>
2025-05-05 13:30:17 -0700

    add tests for promises

--- ts-grip/src/transform-spec.ts
+++ ts-grip/src/transform-spec.ts
@@ -3,6 +3,8 @@ import assert from "node:assert/strict";
 import {transformGrip} from "./transformGrip";
 import {valueGrip} from "./valueGrip";
 
+import {isPromise} from "./util";
+
 describe('transformGrip', () => {
   spec('can convert from String to Int', () => {
     const grip1 = valueGrip('a')
@@ -44,4 +46,44 @@ describe('transformGrip', () => {
     assert.equal(grip2.value, 1965)
     assert.equal(grip1.value, '7ad')
   })
+
+  spec('can transform async values', async () => {
+    const grip1 = valueGrip(Promise.resolve('a'))
+    const grip2 = transformGrip(grip1, {
+      in: (v: Promise<number>) => v.then(v => v.toString(16)),
+      out: (v: Promise<string>) => v.then(v => parseInt(v, 16))
+    })
+
+    assert.equal(await grip2.value, 10)
+    assert.equal(await grip1.value, 'a')
+
+    await grip2.set(Promise.resolve(15))
+    assert.equal(await grip2.value, 15)
+    assert.equal(await grip1.value, 'f')
+
+    await grip2.set(Promise.resolve(1965))
+    assert.equal(await grip2.value, 1965)
+    assert.equal(await grip1.value, '7ad')
+  })
+  spec('can transform non-promise values', async () => {
+    const grip1 = valueGrip(Promise.resolve('a'))
+    const grip2 = transformGrip(grip1, {
+      in: (v: Promise<number>|number) => isPromise(v)
+        ? v.then(v => v.toString(16))
+        : Promise.resolve(v.toString(16)),
+      out: (v: Promise<string>) => v.then(v => parseInt(v, 16))
+    })
+
+    assert.equal(await grip2.value, 10)
+    assert.equal(await grip1.value, 'a')
+
+    await grip2.set(Promise.resolve(15))
+    assert.equal(await grip2.value, 15)
+    assert.equal(await grip1.value, 'f')
+
+    await grip2.set(1965)
+    assert.equal(await grip2.value, 1965)
+    assert.equal(await grip1.value, '7ad')
+  })
+
 })
